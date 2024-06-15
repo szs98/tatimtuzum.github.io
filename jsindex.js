52efa66538d981ea8720709e16479eed6a8bd0ef
szs98 <tncrydn@gmail.com>
2024-06-15 23:42:59 +0300

    Update jsindex.js

--- jsindex.js
+++ jsindex.js
@@ -1,5 +1,5 @@
-var data=fetch("tarifveri.json")
-then=(response=>response.json())
+var data=fetch("tarifveri.json");
+then=(response=>response.json());
 then(veri=>{
-  console.log(veri.tarif)
-})
+  console.log(veri.tarif);
+});
