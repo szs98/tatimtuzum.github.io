a1da30a75780140c6439e46e39a91733dc26f6ce
szs98 <tncrydn@gmail.com>
2024-07-16 13:43:22 +0300

    Update jsindex.js

--- jsindex.js
+++ jsindex.js
@@ -6,14 +6,14 @@
 //);
 var data=fetch("tarifveri.json");
 then=(response=>response.json);
-then(veri=>{
+then(data=>{
   console.log(veri.tarif);
 });
 function a(){
   const msg = document.getElementById("msj");
   const ptext= document.getElementById("yazi");
   try {
-    ptext.innerHTML = veri.tarif.msj.value
+    ptext.innerHTML = data.tarif.msj.value
   }
   catch {
     alert("Tarif bulunamadı")
